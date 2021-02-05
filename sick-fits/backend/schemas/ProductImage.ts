f69e0e8b3267bd969faf9ab67671f160ce4b93dc
Ryan <ryan3738@gmail.com>
2021-02-05 15:17:27 -0800

    creating two way data relationships with keystone

--- sick-fits/backend/schemas/ProductImage.ts
+++ sick-fits/backend/schemas/ProductImage.ts
@@ -1,4 +1,4 @@
-import { text } from '@keystone-next/fields';
+import { text, relationship } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
 import { cloudinaryImage } from '@keystone-next/cloudinary';
 import 'dotenv/config';
@@ -17,5 +17,11 @@ export const ProductImage = list({
       label: 'Source',
     }),
     altText: text(),
+    product: relationship({ ref: 'Product.photo' }),
+  },
+  ui: {
+    listView: {
+      initialColumns: ['image', 'altText', 'product'],
+    },
   },
 });
