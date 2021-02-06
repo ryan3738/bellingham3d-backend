62f836ad93259fa9369965d45b8d72310715524f
Ryan <ryan3738@gmail.com>
2021-02-06 10:44:13 -0800

    setting up apollo client

--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -5,6 +5,7 @@ import { createUploadLink } from 'apollo-upload-client';
 import withApollo from 'next-with-apollo';
 import { endpoint, prodEndpoint } from '../config';
 
+// Custom apollo client that will be injected into front end
 function createClient({ headers, initialState }) {
   return new ApolloClient({
     link: ApolloLink.from([
--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,6 +1,8 @@
 import NProgress from 'nprogress';
 import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
+import withData from '../lib/withData';
 
 // TODO: Swap with our own
 import 'nprogress/nprogress.css';
@@ -10,10 +12,25 @@ Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
+  console.log(apollo);
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+// Apollo client setup
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
