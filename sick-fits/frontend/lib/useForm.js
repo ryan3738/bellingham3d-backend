b3d90bde7ae288793354804433ae9b9fc9edca10
Ryan <ryan3738@gmail.com>
2021-02-09 09:19:56 -0800

    useEffect

--- sick-fits/frontend/lib/useForm.js
+++ sick-fits/frontend/lib/useForm.js
@@ -1,8 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function useForm(initial = {}) {
   // create a state object for our inputs
   const [inputs, setInputs] = useState(initial);
+  const initialValues = Object.values(initial).join('');
+
+  // Monitors state data and updates
+  useEffect(() => {
+    // This function runs when the things we are watching change
+    setInputs(initial);
+  }, [initialValues]);
 
   //   {
   //       name: 'wes',
