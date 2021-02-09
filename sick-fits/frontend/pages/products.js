dcf6f5872a43ba02ecbf8b9673e0965bab445c23
Ryan <ryan3738@gmail.com>
2021-02-09 10:52:17 -0800

    Pagination

--- sick-fits/frontend/pages/products.js
+++ sick-fits/frontend/pages/products.js
@@ -1,5 +1,12 @@
+import Pagination from '../components/Pagination';
 import Products from '../components/Products';
 
 export default function ProductsPage() {
-  return <Products />;
+  return (
+    <div>
+      <Pagination page={1} />
+      <Products />
+      <Pagination page={1} />
+    </div>
+  );
 }
