31418c51c36c52fc53c8ac308fc1a6c196faf89d
Ryan <ryan3738@gmail.com>
2021-02-10 13:50:08 -0800

    password reset - setting a new password

---
+++ sick-fits/frontend/pages/reset.js
@@ -0,0 +1,19 @@
+import RequestReset from '../components/RequestReset';
+import Reset from '../components/Reset';
+
+export default function ResetPage({ query }) {
+  if (!query?.token) {
+    return (
+      <div>
+        <p>Sorry you must supply a token</p>
+        <RequestReset />
+      </div>
+    );
+  }
+  return (
+    <div>
+      <p>Reset your password {query.token} </p>
+      <Reset token={query.token} />
+    </div>
+  );
+}
