363ddc3681f3fb98b29f0a1a016fa2833792d3ab
Ryan <ryan3738@gmail.com>
2021-02-11 10:51:26 -0800

    setting up our stripe checkout

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -7,6 +7,7 @@ import { useUser } from './User';
 import calcTotalPrice from '../lib/calcTotalPrice';
 import { useCart } from '../lib/cartState';
 import RemoveFromCart from './RemoveFromCart';
+import { Checkout } from './Checkout';
 
 const CartItemStyles = styled.li`
   padding: 1rem 0;
@@ -63,6 +64,7 @@ export default function Cart() {
       </ul>
       <footer>
         <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+        <Checkout />
       </footer>
     </CartStyles>
   );
