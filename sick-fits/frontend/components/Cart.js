f708c258846f5423f44666330151fb8c783eec0a
Ryan <ryan3738@gmail.com>
2021-02-10 23:24:08 -0800

    cart update ss 46

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -9,7 +9,7 @@ import { useCart } from '../lib/cartState';
 
 const CartItemStyles = styled.li`
   padding: 1rem 0;
-  border-bottom: 1px solid var(--lightGray);
+  border-bottom: 1px solid var(--lightGrey);
   display: grid;
   grid-template-columns: auto 1fr auto;
   img {
@@ -34,7 +34,7 @@ function CartItem({ cartItem }) {
       <div>
         <h3>{product.name}</h3>
         <p>
-          {formatMoney(product.price * cartItem.quantity)} -
+          {formatMoney(product.price * cartItem.quantity)}-
           <em>
             {cartItem.quantity} &times; {formatMoney(product.price)} each
           </em>
@@ -52,10 +52,8 @@ export default function Cart() {
     <CartStyles open={cartOpen}>
       <header>
         <Supreme>{me.name}'s Cart</Supreme>
+        <CloseButton onClick={closeCart}>&times;</CloseButton>
       </header>
-      <CloseButton type="button" onClick={closeCart}>
-        &times;
-      </CloseButton>
       <ul>
         {me.cart.map((cartItem) => (
           <CartItem key={cartItem.id} cartItem={cartItem} />
