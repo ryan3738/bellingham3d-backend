62eeef586bc6e2af7dcc03fbc28b47217dd9d78c
Ryan <ryan3738@gmail.com>
2021-02-12 09:15:22 -0800

    65 - more flexible rule based functions

--- sick-fits/backend/access.ts
+++ sick-fits/backend/access.ts
@@ -11,15 +11,34 @@ const generatedPermissions = Object.fromEntries(
   permissionsList.map((permission) => [
     permission,
     function ({ session }: ListAccessArgs) {
-      return !!sesssion?.data.role?.[permission];
+      return !!session?.data.role?.[permission];
     },
   ])
 );
 
-// Permissions check if someone meets a criteria - yes or no.
+// Permissions check if someone meets a criteria - yes or no. Generated permissions and custom permission
 export const permissions = {
   ...generatedPermissions,
   isAwesome({ session }: ListAccessArgs) {
     return session?.data.name.includes('ryan');
+  },
+};
+
+// Rule based functions - logical functions that can be used for list access (types: products, orders, users, etc)
+// Rules can return a boolean -yes or no - or a filter which limits which products they can CRUD
+export const rules = {
+  canManageProducts({ session }: ListAccessArgs) {
+    // 1. Do they have the permission of canManageProducts
+    if (permissions.canManageProducts({ session })) {
+      return true;
+    }
+    // 2. if not, do they owen this item?
+    return { user: { id: session.itemId } };
+  },
+  canReadProducts({ session }: ListAccessArgs) {
+    if (permissions.canManageProducts({ session })) {
+      return true; // They can read everything!
+    }
+    return { status: 'AVAILABLE' };
   },
 };
--- sick-fits/backend/schemas/User.ts
+++ sick-fits/backend/schemas/User.ts
@@ -21,5 +21,9 @@ export const User = list({
       ref: 'Role.assignedTo',
       // TODO: Add access control
     }),
+    products: relationship({
+      ref: 'Product.user',
+      many: true,
+    }),
   },
 });
