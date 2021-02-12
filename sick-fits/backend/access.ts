dfb81227a080c6a40ae4b0b6ec3c082269dae06e
Ryan <ryan3738@gmail.com>
2021-02-11 23:16:26 -0800

    64 - permissions access functions

--- sick-fits/backend/access.ts
+++ sick-fits/backend/access.ts
@@ -1,7 +1,25 @@
 // At it's simplest, the access control returns a yes or no value depending on the users session
 
+import { permissionsList } from './schemas/fields';
 import { ListAccessArgs } from './types';
 
 export function isSignedIn({ session }: ListAccessArgs) {
   return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs) {
+      return !!sesssion?.data.role?.[permission];
+    },
+  ])
+);
+
+// Permissions check if someone meets a criteria - yes or no.
+export const permissions = {
+  ...generatedPermissions,
+  isAwesome({ session }: ListAccessArgs) {
+    return session?.data.name.includes('ryan');
+  },
+};
