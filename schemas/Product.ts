3ddef7343debe2888b3fa5c866603eef8af712c3
Ryan <ryan3738@gmail.com>
2021-08-31 18:16:27 -0700

    change create access from rules to permissions

--- schemas/Product.ts
+++ schemas/Product.ts
@@ -6,12 +6,12 @@ import {
   timestamp,
 } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
+import { rules, permissions } from '../access';
 import { InventoryItem } from './InventoryItem';
-import { rules } from '../access';
 
 export const Product = list({
   access: {
-    create: rules.canManageProducts,
+    create: permissions.canManageProducts,
     read: () => true,
     update: rules.canManageProducts,
     delete: rules.canManageProducts,
