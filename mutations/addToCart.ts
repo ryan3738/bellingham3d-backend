d5eb01ffd8b5cbf7c586a5b74fdee14dcd7590da
Ryan <ryan3738@gmail.com>
2021-08-31 18:01:16 -0700

    update addToCart mutation

--- mutations/addToCart.ts
+++ mutations/addToCart.ts
@@ -1,12 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-// /* eslint-disable */
 import { KeystoneContext } from '@keystone-next/types';
 import { Session } from '../types';
-import { CartItemCreateInput } from '../.keystone/schema-types';
 
 interface Arguments {
   productId: string;
@@ -17,7 +10,7 @@ async function addToCart(
   root: any,
   { productId, productVariantIds }: Arguments,
   context: KeystoneContext
-): Promise<CartItemCreateInput> {
+): Promise<any> {
   console.log('ADDING TO CART!');
   // 1. Query the current user see if they are signed in
   const sesh = context.session as Session;
@@ -31,8 +24,8 @@ async function addToCart(
   // 2. Query the current users cart looking for matching product and variants combo
   const allCartItems = await context.lists.CartItem.findMany({
     where: {
-      user: { id: sesh.itemId },
-      product: { id: productId },
+      user: { id: { equals: sesh.itemId } },
+      product: { id: { equals: productId } },
       variants_every: {
         id_in: productVariantIds,
       },
@@ -47,30 +40,23 @@ async function addToCart(
   // 3. See if the current item is in their cart
   if (existingCartItem) {
     console.log(existingCartItem);
-    console.log(
-      `There are already ${existingCartItem.quantity} items, increment by 1!`
-    );
+    console.log(`There are already ${existingCartItem.quantity}, increment by 1!`);
     // 4. Increment existingCartItem by 1
-    await context.lists.CartItem.updateOne({
-      id: existingCartItem.id,
+    return await context.db.lists.CartItem.updateOne({
+      where: { id: existingCartItem.id },
       data: { quantity: existingCartItem.quantity + 1 },
-      resolveFields: false,
     });
-    return;
   }
-  // 4. if item is not in the cart, create a new cart item!
-
+  // 4. if it isnt, create a new cart item!
   console.log(
     `Adding product ${productId} with variants ${productVariantIds} to cart`
   );
-  // eslint-disable-next-line no-return-await
-  return await context.lists.CartItem.createOne({
+  return await context.db.lists.CartItem.createOne({
     data: {
       product: { connect: { id: productId } },
       user: { connect: { id: sesh.itemId } },
       variants: { connect: variantIdsObject },
     },
-    resolveFields: false,
   });
 }
 
