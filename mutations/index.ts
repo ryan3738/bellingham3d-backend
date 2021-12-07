b6324c7eafbab054f51e4e1929a07839e394361e
Ryan <ryan3738@gmail.com>
2021-12-07 14:48:22 -0800

    change name of variable

--- mutations/index.ts
+++ mutations/index.ts
@@ -3,9 +3,10 @@ import addToCart from './addToCart';
 import checkout from './checkout';
 
 // Make a fake graphql tagged template literal
-const graphql = String.raw;
+const gql = String.raw;
+
 export const extendGraphqlSchema = graphQLSchemaExtension({
-  typeDefs: graphql`
+  typeDefs: gql`
     type Mutation {
       addToCart(productId: ID, variantIds: [ID]): CartItem
       checkout(token: String!, shippingId: ID): Order
